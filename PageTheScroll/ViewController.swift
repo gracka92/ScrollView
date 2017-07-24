a6ea07c3d13347dee1d0632b8f97ff0bced348a5
gracka92 <gracjana.leonowicz@gmail.com>
2017-07-24 09:22:36 +0200

    apka do scrollowania

--- PageTheScroll/ViewController.swift
+++ PageTheScroll/ViewController.swift
@@ -10,15 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    var images = [UIImageView]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+//        var contentWidth: CGFloat = 0.0
+        
+        for x in 0...2 {
+            let image = UIImage(named: "icon\(x).png")
+            let imageView = UIImageView(image: image)
+            images.append(imageView)
+            
+            var newX: CGFloat = 0.0
+            
+            newX = view.frame.midX + view.frame.width * CGFloat(x)
+            
+//            contentWidth += newX
+            
+            scrollView.addSubview(imageView)
+            
+            imageView.frame = CGRect(x: (newX) - 75, y: (view.frame.height / 2) - 75, width: 150, height: 150)
+        }
+        
+        scrollView.contentSize = CGSize(width: view.frame.width * 3 , height: view.frame.height)
+        
+       
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
